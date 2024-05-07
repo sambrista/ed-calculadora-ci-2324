b1689a037a5b653173b38f97892e0888b18e11c7
Alfonso Jiménez <yo@alfonsojimenez.net>
2024-05-07 09:29:06 +0200

    Calculadora comentada

--- src/main/java/dam/ed/Calculadora.java
+++ src/main/java/dam/ed/Calculadora.java
@@ -1,14 +1,30 @@
 package dam.ed;
-public class Calculadora {
 
+/**
+ * Clase Calculadora
+ */
+ public class Calculadora {
+    /**
+     * Suma dos números A y B
+     * @param a Sumando A
+     * @param b Sumando B
+     * @return Resultado de la suma
+     */
     double suma(double a, double b) {
         return a + b;
     }
 
+    /**
+     * Resta dos números
+     * @param minuendo Número del que restar
+     * @param sustraendo Cantidad a restar
+     * @return Resultado de la resta
+     */
     double resta(double minuendo, double sustraendo) {
         return minuendo - sustraendo;
     }
-    /**
+
+     /**
       * Multiplica dos números y devuelve el resultado
       * @param a multiplicando 1
       * @param b multiplicando 2
@@ -18,34 +34,49 @@ public class Calculadora {
         return a * b;
     }
 
+    /**
+     *
+     * @param dividendo Número que dividir
+     * @param divisor   Número por el que dividir
+     * @return  Resultado de la división
+     * @throws OperacionInvalidaException Lanza excepción si el divisor es 0
+     */
     double divide(double dividendo, double divisor) throws OperacionInvalidaException {
         if (divisor == 0) {
             throw new OperacionInvalidaException("El divisor es 0");
         }
+        // Esto es un comentario
         return dividendo / divisor;
     }
 
+    /**
+     * Calcula la enésima potencia de un número
+     * @param base Número a elevar
+     * @param exponente Potencia a la que elevar
+     * @return Resultado de elevar la base al exponente indicado
+     */
     double potencia(double base, double exponente) {
         return Math.pow(base, exponente);
     }
 
+    /**
+     * Excepción cuando se realiza una operación inválida
+     */
     public class OperacionInvalidaException extends RuntimeException {
         public OperacionInvalidaException(String mensaje) {
             super(mensaje);
         }
     }
 
+    /**
+     * Calcula la raíz cuadrada de un número
+     * @param radicando Número del que calcular la raíz
+     * @return Resultado de la operación
+     */
     double raiz(double radicando) {
-        /* ITERACIÓN 1 */
-        // return 1;
-        /* ITERACIÓN 2 */
-        // return radicando;
-        /* ITERACIÓN 3 */
-        // return Math.sqrt(radicando);
         if (radicando < 0) {
             throw new OperacionInvalidaException("El radicando no puede ser negativo");
         }
         return Math.sqrt(radicando);
     }
-    // Test
 }
